7ca263bf035e4360f7857d8084b2909641e71d92
root <root@elbambino.home>
2022-05-11 21:25:11 +0200

    Calculadora de IMC

---
+++ indice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+	
+	printf("----CALCULADORA DE INDICE DE MASA CORPORAL----\n");
+	
+	double peso,altura;
+	int indice;
+	
+	printf("Introduce peso en Kg:\n");
+	scanf("%d", &peso);
+	printf("Introduce altura en cm:\n");
+	scanf("%d", &altura);
+	
+	
+	
+	
+	}
